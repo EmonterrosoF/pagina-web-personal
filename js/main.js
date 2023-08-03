97fe08450abda9017bc9fe0485ae2a1622dcb676
Edras <monterrosoedras@gmail.com>
2023-08-02 23:25:50 -0600

    se agrego indicador para el modulo activo y se agrego menu hamburguesa para el modo responsive

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
+const navLinks = document.querySelectorAll('.header-link')
 
-
-
-
-const navLinks = document.querySelectorAll(".header-link")
-
-
-navLinks.forEach(link => {
-
-    link.addEventListener("click", ()=> {
-        navLinks.forEach(link => {
-            link.classList.remove("header-link__active")
-        })
-        
-        link.classList.add("header-link__active")
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.forEach((link) => {
+      link.classList.remove('header-link__active')
     })
+
+    link.classList.add('header-link__active')
+    document.querySelector('.hamburger-menu').classList.remove('is_active')
+    document.querySelector('.hamburger').classList.remove('is-active')
+  })
 })
+
+const menu = document.querySelector('.hamburger')
+
+// method
+function toggleMenu(event) {
+  this.classList.toggle('is-active')
+  document.querySelector('.hamburger-menu').classList.toggle('is_active')
+  event.preventDefault()
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false)
